ce500d3338c1c813e0eb930d09b966f1b9f6b6c1
Duy Hoàng Mạnh <duyhm_@Duys-MacBook-Pro.local>
2022-09-28 00:08:22 +0900

    Create table client_news

---
+++ src/database/migrations/0000_00_00_000004_create_client_news_table.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use App\Common\Database\Definition\DatabaseDefs;
+
+/**
+ * client_newsテーブルを作成するマイグレーションクラス。
+ */
+class CreateClientNewsTable extends Migration
+{
+    /**
+     * マイグレーションを実行する。
+     * @return void
+     * @throws Exception
+     */
+    public function up(): void
+    {
+        try {
+            Schema::connection(DatabaseDefs::CONNECTION_NAME_MIGRATION)
+                ->create('client_news', function (Blueprint $table) {
+                    $table->engine = 'InnoDB';
+                    $table->bigIncrements('id');
+                    $table->foreignId('client_id')->constrained('client_users');
+                    $table->string('title', 150);
+                    $table->char('status', 2)->nullable();
+                    $table->string('avatar', 50)->nullable();
+                    $table->string('content', 5000)->nullable();
+                    $table->timestamp('closed_at')->nullable();
+                    $table->timestamp('published_at')->nullable();
+                    $table->timestamps();
+                    $table->softDeletes();
+                });
+
+            DB::connection(DatabaseDefs::CONNECTION_NAME_MIGRATION)
+                ->statement('ALTER TABLE `client_news` ROW_FORMAT=DYNAMIC;');
+            DB::connection(DatabaseDefs::CONNECTION_NAME_MIGRATION)
+                ->statement('ALTER TABLE `client_news` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;');
+        }
+        catch (PDOException $e) {
+            $this->down();
+            throw $e;
+        }
+    }
+
+    /**
+     * マイグレーションをロールバックする。
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::connection(DatabaseDefs::CONNECTION_NAME_MIGRATION)
+            ->dropIfExists('client_news');
+    }
+}
