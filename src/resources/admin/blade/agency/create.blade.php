2a4116240a0190469807c1df69345204ebdb344c
Truong Quang Thang（チュオン・クアン・タン） <thang.truong@linkbal.co.jp>
2022-11-08 00:23:41 +0900

    Add required-mark

--- src/resources/admin/blade/agency/create.blade.php
+++ src/resources/admin/blade/agency/create.blade.php
@@ -9,7 +9,7 @@
             Agency Create
         </h3>
     </div>
-    <div class="row">
+    <div class="common-form row">
         <div class="col-lg-12">
             <div class="card">
                 <div class="card-body">
@@ -17,21 +17,21 @@
                     <form method="POST" action="{{ route('admin.agency.createConfirm') }}">
                         @csrf
                         <div class="mb-3 row">
-                            <label class="col-sm-2 fw-bold">Agency Name</label>
+                            <label class="col-sm-2 fw-bold required-mark">Agency Name</label>
                             <div class="col-sm-10">
                                 <input type="text" name="name" class="form-control" placeholder="Nguyễn Văn A" required value="{{ Renderer::oldOrElse('name', $agency) }}">
                             </div>
                         </div>
                         <div class="line"></div><br>
                         <div class="mb-3 row">
-                            <label class="col-sm-2 fw-bold">Tel</label>
+                            <label class="col-sm-2 fw-bold required-mark">Tel</label>
                             <div class="col-sm-10">
                                 <input type="text" name="tel" placeholder="0123456789" class="form-control" required value="{{ Renderer::oldOrElse('tel', $agency) }}">
                             </div>
                         </div>
                         <div class="line"></div><br>
                         <div class="mb-3 row">
-                            <label class="col-sm-2 fw-bold">Address</label>
+                            <label class="col-sm-2 fw-bold required-mark">Address</label>
                             <div class="col-sm-10">
                                 <input type="text" name="address" placeholder="Số 1 Đ. Trần Duy Hưng, Trung Hoà, Quận Cầu Giấy, TP Hà Nội" class="form-control" required value="{{ Renderer::oldOrElse('address', $agency) }}">
                             </div>
