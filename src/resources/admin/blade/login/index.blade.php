0b4153e2b237bb5eeb31b2a501b80d1b108ce387
thiennguyen96841996 <thiennguyen96841996@gmail.com>
2022-11-20 16:12:20 +0900

    Display validate login screen

--- src/resources/admin/blade/login/index.blade.php
+++ src/resources/admin/blade/login/index.blade.php
@@ -14,21 +14,28 @@
                     <div class="mb-4">
                         <img class="brand" style="max-width: 100px;" src="{{ busting('img/logo.png', 'admin') }}" alt="bootstraper logo">
                     </div>
-                    <h6 class="mb-4 text-muted">Login to your account</h6>
+                    <h6 class="mb-4 text-muted">Đăng nhập vào tài khoản của bạn</h6>
+                    @if(!empty($errors->getMessages()))
+                        <div class="text text-danger">
+                            @foreach(Arr::collapse($errors->getMessages()) as $msg)
+                                <p class="">{{ $msg }}</p>
+                            @endforeach
+                        </div>
+                    @endif
                     <form action="{{ route('admin.login.login') }}" method="post" enctype="application/x-www-form-urlencoded">
                         @csrf
-                        <div class="mb-3 text-start">
-                            <label for="email" class="form-label">Email adress</label>
-                            <input name ="email" type="email" class="form-control" placeholder="Enter Email" required>
+                        <div class="mb-3 text-start {!! !$errors->has('email') ?: 'text text-danger' !!}">
+                            <label for="email" class="form-label">Email</label>
+                            <input name ="email" type="email" class="form-control {!! !$errors->has('email') ?: 'is-invalid' !!}" placeholder="Nhập Email">
                         </div>
-                        <div class="mb-3 text-start">
-                            <label for="password" class="form-label">Password</label>
-                            <input name="password" type="password" class="form-control" placeholder="Password" required>
+                        <div class="mb-3 text-start {!! !$errors->has('password') ?: 'text text-danger' !!}">
+                            <label for="password" class="form-label">Mật khẩu</label>
+                            <input name="password" type="password" class="form-control {!! !$errors->has('password') ?: 'is-invalid' !!}" placeholder="Nhập mật khẩu">
                         </div>
-                        <button class="btn btn-primary shadow-2 mb-4">Login</button>
+                        <button class="btn btn-primary shadow-2 mb-4">Đăng nhập</button>
                     </form>
-                    <p class="mb-2 text-muted">Forgot password? <a href="forgot-password.html">Reset</a></p>
-                    <p class="mb-0 text-muted">Don't have account yet? <a href="signup.html">Signup</a></p>
+                    <p class="mb-2 text-muted">Quên mật khẩu? <a href="forgot-password.html">Reset</a></p>
+                    <p class="mb-0 text-muted">Chưa có tài khoản? <a href="signup.html">Đăng ký</a></p>
                 </div>
             </div>
         </div>
